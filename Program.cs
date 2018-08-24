d4f8d44f026cce23d243c9922f9d6ac33c58faed
Aaron Miller <millerac333@gmail.com>
2018-08-24 15:53:41 -0500

    added files

--- Program.cs
+++ Program.cs
@@ -1,25 +1,21 @@
-﻿using System;
+﻿// Create a custom type for Employee. An employee has the following properties.
+// First name (string)
+// Last name (string)
+// Title (string)
+// Start date (DateTime)
+// Create a custom type for Company. A company has the following properties.
+// Date founded (DateTime)
+// Company name (string)
+// Employees (List<Employee>)
+// The Company class should also have a ListEmployees() method which outputs the name of each employee to the console.
+// In the Main method of your console application, create a new instance of Company, and three instances of Employee. Then assign the employees to the company.
+
+// You will define the employee type.
+
+using System;
 
 namespace classes
 {
-    public class Company
-    {
-
-        // Some readonly properties (let's talk about gets, baby)
-        public string Name { get; }
-        public DateTime CreatedOn { get; }
-
-        // Create a public property for holding a list of current employees
-
-        /*
-            Create a constructor method that accepts two arguments:
-                1. The name of the company
-                2. The date it was created
-
-            The constructor will set the value of the public properties
-        */
-    }
-
     class Program
     {
         static void Main(string[] args)
