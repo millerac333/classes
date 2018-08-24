0ed6fda410371f4f853252f6f2d1c7243af151dc
Aaron Miller <millerac333@gmail.com>
2018-08-24 15:39:30 -0500

    copied code

--- Program.cs
+++ Program.cs
@@ -2,11 +2,37 @@
 
 namespace classes
 {
+    public class Company
+    {
+
+        // Some readonly properties (let's talk about gets, baby)
+        public string Name { get; }
+        public DateTime CreatedOn { get; }
+
+        // Create a public property for holding a list of current employees
+
+        /*
+            Create a constructor method that accepts two arguments:
+                1. The name of the company
+                2. The date it was created
+
+            The constructor will set the value of the public properties
+        */
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create an instance of a company. Name it whatever you like.
+
+            // Create three employees
+
+            // Assign the employees to the company
+
+            /*
+                Iterate the company's employee list and generate the
+                simple report shown above
+            */
         }
     }
-}
